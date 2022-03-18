fe0fde608bda446ab12ae490c91f02269f842bd9
Pete <peteburquette@gmail.com>
2022-03-18 19:46:24 +0000

    first commit

---
+++ model/data.js
@@ -0,0 +1,16 @@
+// All of these artists are at https://pixabay.com/music/search/mood/laid%20back/
+const songs = [
+  {
+    title: 'Brown 900',
+    artist: 'LC MOD',
+    // audioSrc: cali,
+    artwork: require('../assets/artwork/artwork.jpg'),
+    url: require('../assets/songs/Brown_900Hz_LC_Noise_MOD.mp3'),
+    // url: "https://p.scdn.co/mp3-preview/ef83054a53d976a0e5e947b39cff362a2db7c631?cid=1d62fc4c9282424c8d5611d95669ba0d",
+    // color: "#ffb77a",
+    id: '1',
+    duration: 732,
+  },
+];
+
+export default songs;
